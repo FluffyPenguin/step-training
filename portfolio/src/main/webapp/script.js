e143965944be35097f719e66d57bfcef643e9b3f
Alex Kim <alexkimt@gmail.com>
2020-05-26 18:48:13 -0400

    Remove unecessary elements and comments

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -118,7 +118,8 @@ const checkKonami = (e) => {
     // }, 500);
   }
   //console.log(konamiCheck);
-}; //end check Konami/
+}; //end check Konami
+
 //penguin easter egg
 const checkPenguin = (e) => {
   penguinCheck.push(e.code);
@@ -140,37 +141,4 @@ window.addEventListener("keydown", (e) => {
 });
 
 
-
-
-
-//JS Includes from w3 schools
-/* DOESNT WORK ON LOCAL FILES BC SECURITY ISSUES
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  // Loop through a collection of all HTML elements:
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    //search for elements with a certain atrribute:
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      // Make an HTTP request using the attribute value as the file name:
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          // Remove the attribute, and call this function once more:
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      // Exit the function:
-      return;
-    }
-  }
-}
-*/
 
