976cbefa3e4feca3042c29218f511bda4a9f456e
Alex Kim <alexkimt@gmail.com>
2020-06-13 00:12:04 +0000

    Fix static import of assertEquals.

--- walkthroughs/week-5-tdd/intro/src/test/java/com/google/sps/GreeterTest.java
+++ walkthroughs/week-5-tdd/intro/src/test/java/com/google/sps/GreeterTest.java
@@ -14,7 +14,7 @@
 
 package com.google.sps;
 
-import static org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
--- walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
+++ walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
@@ -19,7 +19,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import static org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.Ignore;
